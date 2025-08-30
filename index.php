d9f747cf22b05836e8c507fb4f8fe09f5407c841
sivabalaji-tn <sivathetechie24@gmail.com>
2025-08-30 13:22:17 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bus Tracking Portal</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+</head>
+<body>
+        <h1>Welcome to the First official Project Bus Tracking System<h1>
+</body>
+</html>
