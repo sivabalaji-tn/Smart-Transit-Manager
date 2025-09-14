a6d2d59fd53f0100f9cf14a0b4923a0fbeb13837
sivabalaji-tn <sivathetechie24@gmail.com>
2025-09-14 22:46:53 +0530

    Fixed admin create page

--- admin/admin-create.php
+++ admin/admin-create.php
@@ -50,7 +50,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create New Admin</title>
+    <title>Create Admin</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
